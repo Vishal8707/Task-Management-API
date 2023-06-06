d1f2a4afa2d7ac16b9731998f1569af35757bc40
Vishal8707 <360vishalsharma.mech@gmail.com>
2023-06-06 21:41:55 +0530

    next day

--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
 const express = require('express')
+require('dotenv').config();
 const route = require('./routes/route')
 const mongoose = require('mongoose')
 
-
-
-
 const app = express()
+
 app.use(express.json())
 
 
-mongoose.connect("mongodb+srv://Vishal:mYcV1yxNYaDMQhzb@cluster0.u3m1d3u.mongodb.net/TaskManagement", {
-    useNewUrlParser: true
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
 })
-    .then(() => console.log("MongoDB is connected"))
-    .catch(err => console.log(err))
+    .then(() => {console.log("MongoDB is connected")})
+    .catch((err) => {console.log(err.message)})
 
 app.use("/", route)
 
-app.listen(3000, function () {
-    console.log("Express app running on port 3000")
+app.listen(process.env.PORT || 5000, function () {
+    console.log("Express app running on port 5000")
 })
